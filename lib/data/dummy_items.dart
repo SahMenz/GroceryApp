106e02f04b7acd9172cc0e4d9ea456706e3b4619
Sah Menz <mensahemma670@gmail.com>
2024-02-20 11:05:30 +0100

    Second commit

---
+++ lib/data/dummy_items.dart
@@ -0,0 +1,23 @@
+//            DIDNT USE ATT THE END, SO IN OTHERWORDS U CAN DELETE IT
+
+import 'package:flutter_form/models/grocery_item.dart';
+import 'package:flutter_form/models/category.dart';
+import 'package:flutter_form/data/categories.dart';
+
+  final groceryItems = [
+  GroceryItem(
+      id: 'a',
+      name: 'Milk',
+      quantity: 1,
+      category: categories[Categories.dairy]!),
+  GroceryItem(
+      id: 'b',
+      name: 'Bananas',
+      quantity: 5,
+      category: categories[Categories.fruit]!),
+  GroceryItem(
+      id: 'c',
+      name: 'Beef Steak',
+      quantity: 1,
+      category: categories[Categories.meat]!),
+];
